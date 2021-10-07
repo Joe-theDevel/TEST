ea570fc3ff12f2dd2e4f4b09873f4b01894aa1b6
Joe-theDevel <92081635+Joe-theDevel@users.noreply.github.com>
2021-10-07 09:31:22 -0500

    Update ArrayScript.js

--- ArrayScript.js
+++ ArrayScript.js
@@ -1,3 +1,7 @@
+// JavaScript "ARRAY GENERATOR" 
+// This file Complements the main script (similar to a C/C++ "header" (include) file.h). 
+// To load & execute use "fetch" & "eval"
+
 var LDR = 
       [
       "aaa", "bbb", "ccc",
